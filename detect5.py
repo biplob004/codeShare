1f9fd2a5196d2efcc8bca9874e7b10b5866f8353
Biplob <35182559+biplob004@users.noreply.github.com>
2021-05-12 01:23:57 +0530

    Update detect5.py

--- detect5.py
+++ detect5.py
@@ -132,7 +132,7 @@ def detect(opt):
                         y2 = int(xyxy[3].item())
 
                         if cls==0 and conf> 0.35: # person
-                        	file_name = 'sohasyolov5/obj_train_data/labels/train/'+save_path.split('/')[-1].split('.')[0]+'txt'
+                            file_name = 'sohasyolov5/obj_train_data/labels/train/'+save_path.split('/')[-1].split('.')[0]+'txt'
                             print('saved label at: '+ file_name)
 #                         	with open(file_name, 'a') as a:
 #                         		a.write(f'7 {x1} {y1} {x2-x1} {y2-y1}')
